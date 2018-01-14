7bf30eba6d43a3062d7444f5b62dee6689e5dc51
Paola  Andrea David <Andree709@hotmail.com>
2018-01-13 22:42:35 -0500

    se ajusta seguridad

--- src/main/java/com/enterprise/usco/uvscannerservidor/UvScannerServidorAplicacion.java
+++ src/main/java/com/enterprise/usco/uvscannerservidor/UvScannerServidorAplicacion.java
@@ -2,14 +2,18 @@ package com.enterprise.usco.uvscannerservidor;
 
 
 
+import com.enterprise.usco.uvscannerservidor.auth.WebSecurityConfig;
 import org.hibernate.SessionFactory;
 import org.hibernate.ejb.HibernateEntityManagerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Import(WebSecurityConfig.class)
 public class UvScannerServidorAplicacion {
 
     public static void main(String[] args) {
--- src/main/java/com/enterprise/usco/uvscannerservidor/controller/AccessController.java
+++ src/main/java/com/enterprise/usco/uvscannerservidor/controller/AccessController.java
@@ -5,19 +5,24 @@
  */
 package com.enterprise.usco.uvscannerservidor.controller;
 
+import com.enterprise.usco.uvscannerservidor.auth.SecurityService;
 import com.enterprise.usco.uvscannerservidor.data.Usuario;
+import org.springframework.web.servlet.ModelAndView;
 import com.enterprise.usco.uvscannerservidor.data.util.ExtJSReturnUtil;
 import com.enterprise.usco.uvscannerservidor.data.util.JsonRequestMappingUtil;
-import com.enterprise.usco.uvscannerservidor.repository.UVRadiationTrackRepository;
 import com.enterprise.usco.uvscannerservidor.repository.UsuarioRepository;
+import com.enterprise.usco.uvscannerservidor.util.AuthUtil;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,35 +34,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableScheduling
 @JsonRequestMappingUtil("/accesscontroller")
 public class AccessController {
-    
+
     private static final Log log = LogFactory.getLog(AccessController.class);
-    
+
     @Autowired//anotacion que permino no inicializar el repository
     private UsuarioRepository usuarioRepository;
-    
+
+    @Autowired
+    protected SecurityService securityService;
+
     @JsonRequestMappingUtil(value = "/login", method = RequestMethod.POST)//declara la direccion del metodo y cuales es el tipo de peticion que se debe usar
     public @ResponseBody
     Map<String, Object> login(@RequestParam(value = "correo", required = true) String correo,
             @RequestParam(value = "password", required = true) String password) {//se comunica extjs (vista)
         List<Usuario> correos = usuarioRepository.findUserByPasswordAndCorreo(correo, password);
         if (correos.size() > 0) {
+            securityService.login(correos.stream().findFirst().orElse(null), Boolean.TRUE);
             return ExtJSReturnUtil.mapOK(correos);
         } else {
             return ExtJSReturnUtil.mapError("usuarioNoRegistrado");
         }
     }
-    
+    @JsonRequestMappingUtil(value = "/logout", method = RequestMethod.GET)//declara la direccion del metodo y cuales es el tipo de peticion que se debe usar
+    public @ResponseBody
+    ModelAndView logout(HttpServletRequest request,HttpServletResponse response) {//se comunica extjs (vista)
+       securityService.logout(request, response);
+       return new ModelAndView("redirect:/");
+    }
+
     @JsonRequestMappingUtil(value = "/register", method = RequestMethod.POST)//declara la direccion del metodo y cuales es el tipo de peticion que se debe usar
-    
+
     public @ResponseBody
     Map<String, Object> register(@RequestParam(value = "user", required = true) String user,//parametros que deben estar presente, no puede ser nulos
             @RequestParam(value = "password", required = true) String password,
             @RequestParam(value = "correo", required = true) String correo,
             @RequestParam(value = "nombre", required = true) String nombre) {//se comunica extjs (vista)
-        
+
         List<Usuario> usuarios = usuarioRepository.findUserByUserName(user);//trae todos los usuarios que tengan el mismo nombre ingersado
         if (usuarios.size() > 0) {
-            
+
             return ExtJSReturnUtil.mapError("usuarioExistente");//
         }
         Usuario usuario = new Usuario(nombre, password, correo, user);
@@ -68,12 +83,31 @@ public class AccessController {
         log.info(user + password);
         usuarioRepository.save(usuario);
         return ExtJSReturnUtil.mapOk(user);
-        
+
     }
 
     private static class Correo {
 
         public Correo() {
+        }
+    }
+
+    @JsonRequestMappingUtil(value = "/verificarSesion", method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> verificarSesion() {
+        Authentication auth = AuthUtil.getCurrentAuth();
+        if (auth != null) {
+            Usuario user = AuthUtil.getCurrentUser();
+            //Usuario userDB = usuarioServicio.findUsuarioByEmail(user.getCorreo());
+            if (user != null) {
+                return ExtJSReturnUtil.mapOK(user);
+
+            } else {
+                return ExtJSReturnUtil.mapError("No se ha iniciado sesión");
+            }
+
+        } else {
+            return ExtJSReturnUtil.mapError("Sesion no encontrada");
         }
     }
 }
